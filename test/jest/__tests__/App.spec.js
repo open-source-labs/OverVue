7288023a29ce069b094105256a5450d22b8383bb
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-07-15 16:57:55 -0700

    altered test file

--- test/jest/__tests__/App.spec.js
+++ test/jest/__tests__/App.spec.js
@@ -7,30 +7,39 @@ import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
 import * as All from 'quasar'
 const { Quasar, date } = All
 
-import mutations from '../../../src/store/mutations';
+import mutations from '../../../src/store/mutations'
+import actions from '../../../src/store/actions'
 
-describe('Test mutations to remove action from component and userActions', () => {
-  const state = {
-    componentMap: {
-      component: {
-        componentName: 'App',
-        children: ['HomeView'],
-        htmlList: [],
-        mapActions: ['action']
-      }
-    },
-    activeComponent: 'component',
-    userActions: ['action']
-  }
+describe('Test mutations + actions to remove action from component and userActions', () => {
+  let state
+  beforeEach(() => {
+    state = {
+      componentMap: {
+        component: {
+          componentName: 'App',
+          children: ['HomeView'],
+          htmlList: [],
+          mapActions: ['action']
+        }
+      },
+      activeComponent: 'component',
+      userActions: ['action']
+    }
+  })
 
   it('deleting user action from state.userActions', () => {
     mutations.DELETE_USER_ACTIONS(state, 'action')
-    expect(state.userActions.length).toBe(0);
+    expect(state.userActions.length).toBe(0)
   })
 
   it('deleting "action" from componentMap', () => {
-    mutations.REMOVE_ACTION_FROM_COMPONENT(state, 'action');
+    mutations.REMOVE_ACTION_FROM_COMPONENT(state, 'action')
     expect(state.componentMap.component.mapActions.length).toBe(0)
   })
-})
 
+  // it('deleting "action" from active component and userActions', () => {
+  //   actions.deleteUserActions('action')
+  //   expect(state.componentMap.component.mapActions.length).toBe(0)
+  //   expect(state.userActions.length).toBe(0)
+  // })
+})
