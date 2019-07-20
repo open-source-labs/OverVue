aadef8f961bcfe10e275e7b4ae89ea512b5bb862
Drew Nguyen <drew.ktran@gmail.com>
2019-07-20 13:56:23 -0700

    created auth/profile pages

--- quasar.conf.js
+++ quasar.conf.js
@@ -5,7 +5,7 @@ module.exports = function (ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
-    boot: ['axios', 'amplify', 'appsync'],
+    boot: ['axios', 'amplify'],
 
     css: ['app.styl'],
 
