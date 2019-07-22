5c5f7a340f53f897aa77f8311fe3c56751e4075d
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-07-22 12:16:43 -0700

    added queries

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    owner
+    date
+    description
+    completed
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    owner
+    date
+    description
+    completed
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    owner
+    date
+    description
+    completed
+  }
+}
+`;
