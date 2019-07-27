d73c3720cf243d76e89ae200164a60fbaee19afd
deanfchung <deanthechung@gmail.com>
2019-07-26 19:53:12 -0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,6 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
-
     // allow console.log during development only
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
