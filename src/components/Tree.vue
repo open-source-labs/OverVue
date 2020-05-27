a9f87487506e117e2bbd041424bd35824fb3d6bb
LolDragoon <Alex_Havas@hotmail.com>
2020-05-27 18:38:05 -0400

    Deleting children no longer breaks the tree graph

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -40,6 +40,7 @@ export default {
           children: compData.children
         })
       })
+      console.log("Formatcomponent map result", result)
       return result
     },
     transformToTree (data) {
