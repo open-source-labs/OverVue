55dc8d2341174050f35b62d0117b9d1514ad511e
LolDragoon <Alex_Havas@hotmail.com>
2020-06-03 11:11:16 -0400

    made find parent function in mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -8,6 +8,7 @@ const cloneDeep = require('lodash.clonedeep')
 import localforage from 'localforage'
 
 //we have to do a search because undo/redo saves payloads as deep clones so passing a memory ref would be detrimental
+// This will find you the actual object by ID
 const breadthFirstSearch = (array,id) => {
   let queue = [...array.filter(el => typeof el === 'object')];
   while(queue.length){
@@ -20,6 +21,23 @@ const breadthFirstSearch = (array,id) => {
       queue.push(...evaluated.children)
       }
     }
+  }
+  console.log("We shouldn't be ever getting here, how did you even search an id that didn't exist?")
+}
+
+//this would find you the parent of a given id
+const breadthFirstSearchParent = (array,id) => {
+  let queue = [...array.filter(el => typeof el === 'object')];
+  while(queue.length){
+    let evaluated = queue.shift()
+    for(let i=0; i<evaluated.children.length;i++){
+    if(evaluated.children[i] === id){
+      return evaluated
+     }
+    }
+    if (evaluated.children.length){
+      queue.push(...evaluated.children)
+      }
   }
   console.log("We shouldn't be ever getting here, how did you even search an id that didn't exist?")
 }
