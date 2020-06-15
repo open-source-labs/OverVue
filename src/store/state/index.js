5c514044535a3b927ac39d271ed2b8efe5c3f6da
Keriann <keriann.lin@gmail.com>
2020-06-15 10:53:57 -0400

    comment fixes

--- src/store/state/index.js
+++ src/store/state/index.js
@@ -2,36 +2,10 @@ import icons from './icons'
 import htmlElementMap from './htmlElementMap'
 const cloneDeep = require('lodash.clonedeep')
 
-// let hardA = {
-//   componentName: "a",
-//   x: 0,
-//   y: 20,
-//   z: 0,
-//   w: 200,
-//   h: 200,
-//   htmlList: [{
-//     children:[{
-//       children:[{
-//         text: 'button',
-//         children:[],
-//         id:0
-//       }],
-//       text:'form',
-//       id:2
-//     }],
-//     text: "div",
-//     id:1
-//   }],
-//   children: [],
-//   parent: {},
-//   isActive: false
-// }
-
 const newState = {
   icons,
   htmlElementMap,
-  // every single time we create a component
-  // sent to export project component
+  // state below is what is used when exporting a project
   componentMap: {
     App: {
       componentName: 'App',
@@ -40,23 +14,19 @@ const newState = {
     },
     HomeView: {
       componentName: 'HomeView',
-      // children: ['a'],
       children: [],
       htmlList: []
     }
-    // NewView: {}
-    // a: hardA
   },
   routes: {
-    // HomeView: [hardA]
     HomeView: []
-    // NewView: []
   },
   userActions: [],
   userState: {},
-  /**
-   *
-   */
+  imagePath: {
+    HomeView: ''
+  },
+  // state below is not used when exporting a project
   componentNameInputValue: '',
   projects: [{ filename: 'Untitled-1', lastSavedLocation: '' }],
 
@@ -64,8 +34,8 @@ const newState = {
   activeComponent: '',
   activeHTML: '',
   activeLayer: {
-    id:'',
-    lineage:[]
+    id: '',
+    lineage: []
   },
 
   selectedElementList: [],
@@ -73,20 +43,17 @@ const newState = {
   activeTab: 0,
   componentChildrenMultiselectValue: [],
   modalOpen: false,
-  parentSelected: false,
-  imagePath: {
-    HomeView: ''
-  }
+  parentSelected: false
 }
 
-//closured method to ensure we only ever write the default state ONCE
+// closured method to ensure we only ever write the default state ONCE
 const writeTheDefault = () => {
   let initial = {}
-  let needsToRun = true;
-  function onced(payload){
-    if(needsToRun){
+  let needsToRun = true
+  function onced (payload) {
+    if (needsToRun) {
       initial = cloneDeep(payload)
-      needsToRun = false;
+      needsToRun = false
     }
     return initial
   }
@@ -95,52 +62,5 @@ const writeTheDefault = () => {
 
 const defaultState = writeTheDefault()
 
-// const defaultState = 
-// {
-//   icons,
-//   htmlElementMap,
-//   // every single time we create a component
-//   // sent to export project component
-//   componentMap: {
-//     App: {
-//       componentName: 'App',
-//       children: ['HomeView'],
-//       htmlList: []
-//     },
-//     HomeView: {
-//       componentName: 'HomeView',
-//       children: [],
-//       htmlList: []
-//     }
-//     // NewView: {}
-//   },
-//   routes: {
-//     HomeView: []
-//     // NewView: []
-//   },
-//   userActions: [],
-//   userState: {},
-//   /**
-//    *
-//    */
-//   componentNameInputValue: '',
-//   projects: [{ filename: 'Untitled-1', lastSavedLocation: '' }],
-//   activeRoute: 'HomeView',
-//   activeComponent: '',
-//   selectedElementList: [],
-//   projectNumber: 2,
-//   activeTab: 0,
-//   componentChildrenMultiselectValue: [],
-//   modalOpen: false,
-//   parentSelected: false,
-//   imagePath: ''
-// }
-
-// const getDefault = ()=>{
-//   return {...defaultState}
-// }
-
-
-
 export default newState
-export {defaultState}
+export { defaultState }
