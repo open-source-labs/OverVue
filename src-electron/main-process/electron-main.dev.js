02f3c0eb47308a4ef7144961c744a3d656eb7170
aleckslu <alecks.lu@gmail.com>
2021-11-06 11:26:47 -0700

    added in dev tools code to boilerplate

--- src-electron/main-process/electron-main.dev.js
+++ src-electron/main-process/electron-main.dev.js
@@ -11,14 +11,14 @@
 require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
-})
+// require('electron').app.on('ready', () => {
+//   let installExtension = require('electron-devtools-installer')
+//   installExtension.default(installExtension.VUEJS_DEVTOOLS)
+//     .then(() => {})
+//     .catch(err => {
+//       console.log('Unable to install `vue-devtools`: \n', err)
+//     })
+// })
 
 // Require `main` process to boot app
 require('./electron-main')
