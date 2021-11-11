80c853643c33ee7c16bf988d40554f915edf5a2f
Sonny Nguyen <87385948+sn163@users.noreply.github.com>
2021-11-10 16:08:04 -0800

    added prebuilt components

---
+++ src/components/prebuilt_components/Img.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  </div>
+</template>
+
+<script>
+
+import VueDraggableResizable from 'vue-draggable-resizable'
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
+export default {
+  name: 'Buttons',
+  components: {
+    VueDraggableResizable
+  },
+pu// Vue calls the data function as part of creating a new component instance. data should return an object. Vue will then store on the component instance as $data.
+  data() {
+    return ({})
+  }
+}
+</script>
