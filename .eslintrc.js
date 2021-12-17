00140840cd165207dedca3d5b875c09ecf963b5f
lamerson28 <82220715+rozay28@users.noreply.github.com>
2021-12-16 20:51:08 -0600

    ComponentTab root updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser',
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
+    requireConfigFile: false
   },
 
   env: {
