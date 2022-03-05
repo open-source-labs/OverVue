97d9153c253f2095f92fcc781b61be22bbd853bb
Julia Bakerink <juliabakerink@gmail.com>
2022-03-04 16:09:09 -0800

    Added functionality to nav bar button, specifically for Slack login, import componenent, and get started.

--- src/components/slack_login/SlackLoginWindow.vue
+++ src/components/slack_login/SlackLoginWindow.vue
@@ -3,28 +3,6 @@
   <div>
     <!-- <div>
       <q-btn class="glossy bg-black openModalBtn" size="5px" @click="openLogin">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          style="height: 18px; width: 18px; margin-right: 0"
-          viewBox="0 0 122.8 122.8"
-        >
-          <path
-            d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
-            fill="#e01e5a"
-          ></path>
-          <path
-            d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
-            fill="#36c5f0"
-          ></path>
-          <path
-            d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
-            fill="#2eb67d"
-          ></path>
-          <path
-            d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
-            fill="#ecb22e"
-          ></path>
-        </svg>
       </q-btn>
     </div> -->
 
@@ -35,6 +13,37 @@
     <!--
         Skip button:
         -->
+                <q-btn
+              class="menu-btn"
+              color= "purple"
+              no-caps
+              @click="openLogin"
+            >
+                 <svg
+                xmlns="http://www.w3.org/2000/svg"
+                style="height: 20px; width: 20px; margin-right: 12px"
+                viewBox="0 0 122.8 122.8"
+              >
+                <path
+                  d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
+                  fill="#e01e5a"
+                ></path>
+                <path 
+                  d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
+                  fill="#36c5f0"
+                ></path>
+                <path
+                  d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
+                  fill="#2eb67d"
+                ></path>
+                <path
+                  d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
+                  fill="#ecb22e"
+                ></path>
+              </svg>
+              Slack Login
+            </q-btn>
+
     <q-dialog
       v-model="showLogin"
       persistent
@@ -87,7 +96,7 @@ export default {
   data() {
     return {
       isAuthenticating: false,
-      showLogin: true,
+      showLogin: false,
       errorMessage: "",
     };
   },
@@ -150,6 +159,7 @@ export default {
       this.errorMessage = "";
     },
     openLogin: function () {
+      console.log('trying to open!')
       this.showLogin = true;
     },
   },
@@ -171,6 +181,7 @@ export default {
   color: $menutext;
   background-color: purple;
   margin-bottom: 10px;
+
 }
 
 #skipButton {
@@ -199,4 +210,10 @@ export default {
 .openModalBtn {
   border: 1px solid #289ead;
 }
+.menu-btn{
+  width: 100%;
+  margin-bottom:20px;
+}
+
+
 </style>
