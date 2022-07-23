860c9eaa10409ea9db78af6358a62652f83290d2
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-07-23 11:09:11 -0700

    Co-authored-by: Johnny Chan <jchan444@users.noreply.github.com>

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -371,7 +371,8 @@ const mutations = {
       text: payload.elementName,
       id: payload.date,
       children: [],
-      class: ''
+      class: '',
+      binding: ''
     });
   },
 
@@ -393,7 +394,8 @@ const mutations = {
       text: payload.elementName,
       id: payload.date,
       children: [],
-      class: ''
+      class: '',
+      binding: ""
     });
   },
 
@@ -435,6 +437,7 @@ const mutations = {
 
   [types.SET_ACTIVE_LAYER]: (state, payload) => {
     const newLayer = cloneDeep(state.activeLayer);
+
     newLayer.lineage.push(payload.text);
     newLayer.id = payload.id;
     state.activeLayer = newLayer;
@@ -492,25 +495,25 @@ const mutations = {
     const idDrag = state.componentMap[componentName].idDrag;
     const idDrop = state.componentMap[componentName].idDrop;
 
-    if(idDrag !== idDrop && idDrag !== '' && idDrop !== '') {
+    if (idDrag !== idDrop && idDrag !== '' && idDrop !== '') {
       let indexDrag;
       let indexDrop;
       const htmlList = state.componentMap[componentName].htmlList.slice(0)
 
       if (state.activeLayer.id === "") {
         htmlList.forEach((el, i) => {
-          if(el.id === idDrag){
+          if (el.id === idDrag) {
             indexDrag = i;
-          } else if (el.id === idDrop){
+          } else if (el.id === idDrop) {
             indexDrop = i;
           }
         })
         const draggedEl = htmlList.splice(indexDrag, 1)[0]
-        htmlList.splice(indexDrop,0,draggedEl)
+        htmlList.splice(indexDrop, 0, draggedEl)
       } else {
         const nestedDrag = breadthFirstSearchParent(htmlList, idDrag);
         const nestedDrop = breadthFirstSearchParent(htmlList, idDrop);
-        let nestedEl =nestedDrag.evaluated.children.splice(nestedDrag.index, 1)[0]
+        let nestedEl = nestedDrag.evaluated.children.splice(nestedDrag.index, 1)[0]
         nestedDrop.evaluated.children.splice(nestedDrop.index, 0, nestedEl)
       }
       state.componentMap[componentName].htmlList = htmlList;
@@ -523,22 +526,22 @@ const mutations = {
     const selectedIdDrag = state.selectedIdDrag;
     const selectedIdDrop = state.selectedIdDrop;
 
-    if(selectedIdDrag !== selectedIdDrop && selectedIdDrag !== '' && selectedIdDrop !== ''){
+    if (selectedIdDrag !== selectedIdDrop && selectedIdDrag !== '' && selectedIdDrop !== '') {
       const htmlList = state.selectedElementList.slice(0)
 
       let indexDrag;
       let indexDrop;
 
       htmlList.forEach((el, i) => {
-        if(el.id === selectedIdDrag){
+        if (el.id === selectedIdDrag) {
           indexDrag = i;
-        } else if (el.id === selectedIdDrop){
+        } else if (el.id === selectedIdDrop) {
           indexDrop = i;
         }
       })
 
       const draggedEl = htmlList.splice(indexDrag, 1)[0]
-      htmlList.splice(indexDrop,0,draggedEl)
+      htmlList.splice(indexDrop, 0, draggedEl)
       state.selectedElementList = htmlList;
     }
     state.selectedIdDrag = '';
@@ -717,10 +720,10 @@ const mutations = {
     const updatedComponent = state.routes[state.activeRoute].filter(
       (element) => element.componentName === payload.activeComponent
     )[0];
-    
+
     updatedComponent.color = payload.color
   },
-//Attribute updater for parent
+  //Attribute updater for parent
   [types.EDIT_ATTRIBUTE]: (state, payload) => {
     const updatedComponent = state.routes[state.activeRoute].filter(
       (element) => element.componentName === payload.activeComponent
@@ -799,10 +802,6 @@ const mutations = {
 
   [types.OPEN_NOTE_MODAL]: (state) => {
     state.noteModalOpen = !state.noteModalOpen;
-  }, 
-  
-  [types.OPEN_COLOR_MODAL]: (state) => {
-    state.colorModalOpen = !state.colorModalOpen;
   },
   //Jace practice for future, not place classList directly in activeComponent
   [types.OPEN_ATTRIBUTE_MODAL]: (state) => {
@@ -810,12 +809,52 @@ const mutations = {
   },
 
   [types.ADD_ACTIVE_COMPONENT_CLASS]: (state, payload) => {
-    state.componentMap[state.activeComponent].htmlList.forEach((el) => {
-      if (payload.id === el.id) {
-        el.class = payload.class
-      }
-    })
+    if (state.activeComponentObj.htmlList)
+
+      state.componentMap[state.activeComponent].htmlList.forEach((el) => {
+        //adding class into it's child 1st layer
+        if (el.children.length !== 0) {
+          el.children.forEach((element) => {
+            if (payload.id === element.id) {
+              element.class = payload.class
+            }
+          })
+        }
+        if (payload.id === el.id) {
+          el.class = payload.class
+        }
+      })
+
   },
+
+  // //add binding 
+  [types.addBindingText]: (state, payload) => {
+    //access the htmlList, add payload to the empty bind obj
+    //const active = state.componentMap[state.activeComponent].htmlList;
+    if (payload.binding === "") {
+      state.componentMap = {
+        ...state.componentMap
+      }
+    } else {
+      const id = payload.id
+
+      if (state.activeComponentObj.htmlList)
+        state.componentMap[state.activeComponent].htmlList.forEach((el) => {
+
+          if (el.children.length !== 0) {
+            el.children.forEach((element) => {
+              if (payload.id === element.id) {
+                element.binding = payload.binding
+              }
+            })
+          }
+          if (payload.id === el.id) {
+            el.binding = payload.binding
+          }
+        })
+    }
+  },
+
   [types.DELETE_ACTIVE_COMPONENT_CLASS]: (state, payload) => {
     state.componentMap[state.activeComponent].classList.forEach((el, ind) => {
       if (payload === el) {
