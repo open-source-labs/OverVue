bba6830edeb94d13bcaa4a592b860078589dce9e
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-08-02 10:46:26 -0700

    new info file made

---
+++ src/components/right-sidebar/tutorial/NewVersionInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <section id="new features">
+    <q-card id="list">
+
+      <h6 class="feature list">What's New?</h6>
+
+      <p>More Semantic HMTL</p>
+      <p>Component Customization</p>
+
+    </q-card>
+  </section>
+</template>
