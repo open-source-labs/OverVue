2747f2729a3875dce0574550889dfe82563e5f21
Linden Young <imlindenyoung@gmail.com>
2022-10-09 20:56:47 -0600

    almost done with options to composition api for HTMLQueue.vue, a few bugs exist

--- src/components/right-sidebar/HTMLQueue.vue
+++ src/components/right-sidebar/HTMLQueue.vue
@@ -40,7 +40,7 @@ Description:
             v-if="
               activeComponent === '' ||
               exceptions.includes(element[0]) ||
-              moreExceptions.includes(element[0])
+              moreExceptions().includes(element[0])
             "
           ></i>
           <i
@@ -60,6 +60,222 @@ Description:
 </template>
 
 <script>
+export default {
+  name: "HTMLQueue",
+};
+</script>
+
+<script setup>
+import { ref, computed, watch } from "vue";
+import { useStore } from "vuex";
+import {
+  setSelectedElementList,
+  deleteSelectedElement,
+  deleteFromComponentHtmlList,
+} from "../../store/types";
+import { breadthFirstSearch } from "../../utils/search.util";
+
+const store = useStore();
+
+const props = defineProps({
+  name: String,
+  listToRender: Array,
+});
+
+const exceptions = ref(["input", "img", "link"]);
+const attributeModal = ref(false);
+const classText = ref("");
+const bindingText = ref("");
+
+const activeComponentObj = computed(() => store.state.activeComponentObj);
+const selectedElementList = computed(() => store.state.selectedElementList);
+const componentMap = computed(() => store.state.componentMap);
+const activeComponent = computed(() => store.state.activeComponent);
+const activeHTML = computed(() => store.state.activeHTML);
+const activeLayer = computed(() => store.state.activeLayer);
+const attributeModalOpen = computed(() => store.state.attributeModalOpen);
+
+const renderList = computed({
+  get() {
+    if (activeComponent.value === "")
+      return selectedElementList.value.map((el, index) => [
+        el.text,
+        index,
+        el.id,
+      ]);
+    // change activeComponent's htmlList into an array of arrays ([element/component name, index in state])
+    if (activeComponent.value !== "" && activeLayer.value.id === "") {
+      let sortedHTML = componentMap.value[activeComponent.value].htmlList
+        .map((el, index) => [el.text, index, el.id, el.z])
+        .filter((el) => {
+          return el[0] !== undefined;
+        });
+      return sortedHTML;
+    }
+    let activeElement = breadthFirstSearch(
+      componentMap.value[activeComponent.value].htmlList,
+      activeLayer.value.id
+    );
+    let sortedHTML = activeElement.children
+      .map((el, index) => [el.text, index, el.id])
+      .filter((el) => {
+        return el[0] !== undefined;
+      });
+    return sortedHTML;
+  },
+  set(value) {
+    store.dispatch(setSelectedElementList, value);
+  },
+});
+
+// computed functions depth and moreExceptions ???
+
+const depth = () => {
+  let newTitle = "";
+  activeLayer.value.lineage.forEach((el) => {
+    newTitle += ` > ${el}`;
+  });
+  return newTitle;
+};
+
+const moreExceptions = () => {
+  let childComponent = [];
+  if (activeComponent.value) {
+    childComponent = componentMap.value[activeComponent.value].children;
+  }
+  return childComponent;
+};
+
+const parentSelected = (payload) => store.dispatch("parentSelected", payload);
+
+const setActiveHTML = (payload) => store.dispatch("setActiveHTML", payload);
+const setActiveLayer = (payload) => store.dispatch("setActiveLayer", payload);
+const upOneLayer = (payload) => store.dispatch("upOneLayer", payload);
+const setSelectedIdDrag = (payload) =>
+  store.dispatch("setSelectedIdDrag", payload);
+const setIdDrag = (payload) => store.dispatch("setIdDrag", payload);
+const setSelectedIdDrop = (payload) =>
+  store.dispatch("setSelectedIdDrop", payload);
+const setIdDrop = (payload) => store.dispatch("setIdDrop", payload);
+const dragDropSortHtmlElements = (payload) =>
+  store.dispatch("dragDropSortHtmlElements", payload);
+const dragDropSortSelectedHtmlElements = (payload) =>
+  store.dispatch("dragDropSortSelectedHtmlElements", payload);
+const openAttributeModal = (payload) =>
+  store.dispatch("openAttributeModal", payload);
+const addActiveComponentClass = (payload) =>
+  store.dispatch("addActiveComponentClass", payload);
+const addBindingText = (payload) => store.dispatch("addBindingText", payload);
+const clearActiveHTML = (payload) => store.dispatch("clearActiveHTML", payload);
+
+const deleteElement = (id) => {
+  if (activeComponent.value === "")
+    store.dispatch(deleteSelectedElement, id[0]);
+  else store.dispatch(deleteFromComponentHtmlList, id[1]);
+};
+
+const closeMenu = (element) => {
+  if (activeComponent.value !== "") {
+    clearActiveHTML();
+    openAttributeModal(element);
+  }
+};
+
+const setActiveElement = (element) => {
+  if (activeComponent.value !== "") {
+    setActiveHTML(element);
+    if (attributeModal.value === false) {
+      openAttributeModal(element);
+    } else {
+      closeMenu(element);
+    }
+  }
+};
+
+const setLayer = (element) => {
+  setActiveLayer(element);
+  element.id = activeHTML.value;
+};
+
+const setParentLayer = () => {
+  if (activeLayer.value.id !== "") {
+    upOneLayer.value(activeLayer.value.id);
+  }
+};
+
+const startDrag = (event, id) => {
+  //add a class to make the html element currently being drag transparent
+  event.target.classList.add("currentlyDragging");
+  const dragId = id;
+  //store the id of dragged element
+  if (activeComponent.value === "") setSelectedIdDrag(dragId);
+  else setIdDrag(dragId);
+};
+
+const dragEnter = (event, id) => {
+  event.preventDefault();
+  const dropId = id;
+  //store the id of the html element whose location the dragged html element could be dropped upon
+  if (activeComponent.value === "") setSelectedIdDrop(dropId);
+  else setIdDrop(dropId);
+};
+
+const dragOver = (event) => {
+  event.preventDefault();
+};
+
+const endDrag = (event) => {
+  //remove the 'currentlyDragging' class after the HTML is dropped to remove transparency
+  event.preventDefault();
+  event.target.classList.remove("currentlyDragging");
+  //invoke the action that will use the idDrag and idDrop to sort the HtmlList
+  if (activeComponent.value === "") dragDropSortSelectedHtmlElements();
+  else dragDropSortHtmlElements();
+};
+
+const submitClass = (element, idNum) => {
+  if (element === "") {
+    return;
+  }
+
+  let payload = {
+    class: element,
+    id: idNum,
+  };
+  addActiveComponentClass(payload);
+  classText.value = "";
+};
+
+const addBinding = (input, idNum) => {
+  if (input === "") {
+    return;
+  }
+  const payload = {
+    binding: input,
+    id: idNum,
+  };
+  addBindingText(payload);
+  bindingText.value = "";
+};
+
+watch(
+  () => attributeModalOpen.value,
+  () => (attributeModal.value = attributeModalOpen.value)
+);
+
+watch(
+  () => activeComponent.value,
+  () => {
+    if (activeComponent.value !== "") {
+      component = true;
+    } else {
+      component = false;
+    }
+  }
+);
+</script>
+
+<!-- <script>
 import { keys } from "localforage";
 import { mapState, mapActions } from "vuex";
 import {
@@ -269,7 +485,7 @@ export default {
     },
   },
 };
-</script>
+</script> -->
 
 <style lang="scss" scoped>
 .html-queue {
