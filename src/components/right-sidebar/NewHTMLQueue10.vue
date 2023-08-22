57670790d2b50949e63b27c693a3e1ea6c33e336
Janica <abagat07@gmail.com>
2023-08-22 14:49:00 -0700

    resolves htmlQueue bug isses  Co-authored by: Janica Abagat abagat07@gmail.com Jaime de Venecia deveneciaj@gmail.com Trisha Duong trishanduong@gmail.com David Lee davidetlee@gmail.com

--- src/components/right-sidebar/NewHTMLQueue10.vue
+++ src/components/right-sidebar/NewHTMLQueue10.vue
@@ -1,29 +1,48 @@
 <!-- [OverVue v.10.0] new & improved HTML Queue (now includes nesting) -->
 
 <template>
-  <button @click="outputTreeData"></button>
-  <Draggable v-model="treeData" />
+  <button @click="outputTreeData">Output Tree Data</button>
+  <div class="top-p" v-if="activeComponent === ''">
+    Select a component to see your HTML elements.
+  </div>
+  <div v-else>
+    <Draggable v-model="treeData" />
+  </div>
 </template>
 
 <script setup>
 // [OverVue v.10.0] he-tree library: https://hetree.phphe.com/v2/
 import { Draggable } from "@he-tree/vue";
-// import "@he-tree/vue/style/default.css";
+import "@he-tree/vue/style/default.css";
 import { useStore } from "src/store/main";
 import { ref, computed, watch } from "vue";
 
 const store = useStore();
 
+const treeData = ref([]);
+
 const componentMap = computed(() => store.componentMap);
 const activeComponent = computed(() => store.activeComponent);
+// const htmlList = computed(() => store.activeComponent.htmlList);
 
-const htmlList = JSON.parse(JSON.stringify(componentMap.value))[
-  activeComponent.value
-].htmlList;
+// const htmlList = computed(() => {
+//   if (activeComponent.value !== "") {
+//     treeData.value = JSON.parse(JSON.stringify(componentMap.value))[
+//       activeComponent.value
+//     ].htmlList;
+//     return treeData.value;
+//   }
+// });
 
-const treeData = ref(
-  JSON.parse(JSON.stringify(componentMap.value))[activeComponent.value].htmlList
-);
+if (activeComponent.value !== "") {
+  treeData.value = JSON.parse(JSON.stringify(componentMap.value))[
+    activeComponent.value
+  ].htmlList;
+}
+
+const outputTreeData = () => {
+  console.log(treeData.value);
+};
 
 // Watching tree data so that state updates when user interacts with tree
 watch(
@@ -55,53 +74,3 @@ watch(
 // convertFromTreeData(htmlList);
 // // console.log("converted", JSON.parse(JSON.stringify(convertedTreeData.value)));
 </script>
-
-<style>
-.he-tree--rtl {
-  direction:rtl
-}
-
-.he-tree--drag-overing {
-  background: #333334;
-}
-
-.he-tree-drag-placeholder {
-  background:#72d096;
-  /* border:1px dashed #00ff80; */
-  height:40px;
-}
-
-.he-tree {
-
-}
-
-.vtlist {
-  display: flex;
-}
-
-.vtlist-inner {
-  gap: 2px;
-  /* display: flex; */
-}
-
-.tree-node {
-  display: flex;
-  /* width: 50px; */
-}
-
-.tree-node-inner {
-  box-sizing: border-box;
-  font-size: 20px;
-  background-color: #72d096;
-  border: 1px solid white;
-  border-radius: 5px;
-  padding: 5px;
-  text-align: center;
-  width: 200px;
-  height: 40px;
-  text-overflow: ellipsis;
-  overflow: hidden;
-}
-
-
-</style>
