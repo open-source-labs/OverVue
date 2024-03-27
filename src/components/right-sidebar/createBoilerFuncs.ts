53b661377e9386e6dce5fbb87dafaa6248aec230
anthonyHerr <103393401+anthonyHerr@users.noreply.github.com>
2024-03-26 19:33:42 -0700

    resloving merge conflicts Co-authored-by: Daniel Garan <g.daniel02@icloud.com> Co-authored-by: Kevin Can <kevincan321@gmail.com> Co-authored-by: Anthony Herrera <herreraanthony2@gmail.com> Co-authored-by: Rob Sinzieri <robsin@gmail.com> Co-authored-by: Roderick de Leon <roderickx.deleon@gmail.com>

--- src/components/right-sidebar/createBoilerFuncs.ts
+++ src/components/right-sidebar/createBoilerFuncs.ts
@@ -16,15 +16,9 @@ export const createBoilerOptions = (componentName: string, children: string[] | 
   App: RouteComponentMap;
   HomeView: RouteComponentMap;
 }) => {
-export const createBoilerOptions = (componentName: string, children: string[] | {
-  [key: string]: RouteComponentMap | Component;
-  App: RouteComponentMap;
-  HomeView: RouteComponentMap;
-}) => {
   // add import mapstate and mapactions if they exist
   let imports = "";
   const activeComp = componentMap.value[activeComponent.value] as Component;
-  // console.log('activeComp in Options is ',activeComp)
   // if (checked.value === false) {
     if (activeComp.actions.length || activeComp.state.length) {
       imports += "import { ";
@@ -75,7 +69,7 @@ export const createBoilerOptions = (componentName: string, children: string[] | 
 
   const vtComponents: string[] = [];
 
-  htmlBinding.forEach((el: { text: string; }) => {
+  htmlBinding.forEach((el) => {
     if (vuetensilsSet.has(el.text)) {
       // Add import statement for Vuetensils components
       vtComponents.push(el.text);
@@ -89,7 +83,7 @@ export const createBoilerOptions = (componentName: string, children: string[] | 
   }
 
   data += "  data() {\n    return {\n";
-  htmlBinding.forEach((el: { binding: string }) => {
+  htmlBinding.forEach((el) => {
     if (el.binding !== "") {
       data += `      ${el.binding}: "PLACEHOLDER FOR VALUE", `;
       data += "\n";
@@ -204,9 +198,6 @@ export const createBoilerComposition = (componentName: string, children: string[
     imports += 'import { defineComponent } from "vue";\n';
   }
 
-let childrenComponentNames = "\n";
-
-if (Array.isArray(children)){
   // add imports for children
   let childrenComponentNames = "\n";
   if (Array.isArray(children)) {
@@ -220,137 +211,136 @@ if (Array.isArray(children)){
     });
   }
 
-// if true add data section and populate with props
-let data = "";
-if (activeComp.props.length) {
-  data += "  props: {";
-  activeComp.props.forEach((prop) => {
-    data += `\n    ${prop}: "PLACEHOLDER FOR VALUE",`;
-  });
-  data += "\n";
-  //data += "    }\n";
-  data += "  },\n";
-}
-
-
-const htmlBinding = componentMap.value[activeComponent.value].htmlList;
-
-// [OverVue v.10.0] add Vuetensils import statements to <script setup>
-
-const vuetensilsSet = new Set(Object.keys(vtIcons));
-
-let vuetensilsImports = "";
-
-const vtComponents: string[] = [];
-
-htmlBinding.forEach((el: { text: string }) => {
-  if (vuetensilsSet.has(el.text)) {
-    // Add import statement for Vuetensils components
-    vtComponents.push(el.text);
+  // if true add data section and populate with props
+  let data = "";
+  if (activeComp.props.length) {
+    data += "  props: {";
+    activeComp.props.forEach((prop) => {
+      data += `\n    ${prop}: "PLACEHOLDER FOR VALUE",`;
+    });
+    data += "\n";
+    //data += "    }\n";
+    data += "  },\n";
   }
-});
 
-if (vtComponents.length) {
-  vuetensilsImports += `import { ${vtComponents.join(
-    ", "
-  )} } from 'vuetensils/src/components';\n`;
-}
+
+  const htmlBinding = componentMap.value[activeComponent.value].htmlList;
+
+  // [OverVue v.10.0] add Vuetensils import statements to <script setup>
+
+  const vuetensilsSet = new Set(Object.keys(vtIcons));
+
+  let vuetensilsImports = "";
+
+  const vtComponents: string[] = [];
+
+  htmlBinding.forEach((el) => {
+    if (vuetensilsSet.has(el.text)) {
+      // Add import statement for Vuetensils components
+      vtComponents.push(el.text);
+    }
+  });
+
+  if (vtComponents.length) {
+    vuetensilsImports += `import { ${vtComponents.join(
+      ", "
+    )} } from 'vuetensils/src/components';\n`;
+  }
 /*------------- setup() function -------------*/
 data += "  setup() {"
-data += "  setup() {"
 if(activeComp.state.length || activeComp.actions.length){
-data += "   \n    const /* testStore */ = useStore();  ";
+  data += "   \n    const /* testStore */ = useStore();  ";
 }
-htmlBinding.forEach((el: { binding: string }) => {
-  if (el.binding !== "") {
-    data += `      ${el.binding}: "PLACEHOLDER FOR VALUE", `;
-    data += "\n";
+  htmlBinding.forEach((el) => {
+    if (el.binding !== "") {
+      data += `      ${el.binding}: "PLACEHOLDER FOR VALUE", `;
+      data += "\n";
+    }
+  });
+
+  let returnStatement = "";
+
+  if (activeComp.state.length || activeComp.actions.length) {
+    returnStatement = "\n    return { \n";
   }
-});
-
-let returnStatement = "";
-
-if (activeComp.state.length || activeComp.actions.length) {
-  returnStatement = "\n    return { \n";
-}
 
 
-// if true add computed section and populate with state
-let computed = "";
-if (activeComp.state.length) {
-  const currState =
-  activeComp.state.forEach((state) => {
-    data += `\n    const ${state} = computed(() => /* testStore */.${state}); `;
-    returnStatement += `      ${state}, \n`
+  // if true add computed section and populate with state
+  let computed = "";
+  if (activeComp.state.length) {
+    const currState =
+    activeComp.state.forEach((state) => {
+      data += `\n    const ${state} = computed(() => /* testStore */.${state}); `;
+      returnStatement += `      ${state}, \n`
 
-  });
-}
+    });
+  }
 
-data += '\n'
+  data += '\n'
 
-// if true add methods section and populate with actions
-let methods = "";
-if (activeComp.actions.length) {
-  activeComp.actions.forEach((action) => {
-    methods += `\n    const ${action} = () => /* testStore */.${action}();`;
-    returnStatement += `      ${action}, \n`
-  });
-}
+  // if true add methods section and populate with actions
+  let methods = "";
+  if (activeComp.actions.length) {
+    activeComp.actions.forEach((action) => {
+      methods += `\n    const ${action} = () => /* testStore */.${action}();`;
+      returnStatement += `      ${action}, \n`
+    });
+  }
 
-let htmlArray = componentMap.value[componentName].htmlList;
-let styleString = "";
-const activeCompObj = activeComponentObj.value as Component; // typed this to fix activeComponentObj.value "is possibly null" error
-if (
-  activeComponentObj.value &&
-  (activeComponentObj.value as Component).htmlAttributes.class !== ""
-) {
-  styleString += `.${
-    (activeComponentObj.value as Component).htmlAttributes.class
-  } { \n\tbackground-color: ${(activeComponentObj.value as Component).color};
+  let htmlArray = componentMap.value[componentName].htmlList;
+  let styleString = "";
+  const activeCompObj = activeComponentObj.value as Component; // typed this to fix activeComponentObj.value "is possibly null" error
+  if (
+    activeComponentObj.value &&
+    (activeComponentObj.value as Component).htmlAttributes.class !== ""
+  ) {
+    styleString += `.${
+      (activeComponentObj.value as Component).htmlAttributes.class
+    } { \n\tbackground-color: ${(activeComponentObj.value as Component).color};
 \tgrid-area: ${
-    (activeComponentObj.value as Component).htmlAttributes.gridArea[0]
-  } / ${
-    (activeComponentObj.value as Component).htmlAttributes.gridArea[1]
-  } / ${
-    (activeComponentObj.value as Component).htmlAttributes.gridArea[2]
-  } / ${(activeComponentObj.value as Component).htmlAttributes.gridArea[3]};
+      (activeComponentObj.value as Component).htmlAttributes.gridArea[0]
+    } / ${
+      (activeComponentObj.value as Component).htmlAttributes.gridArea[1]
+    } / ${
+      (activeComponentObj.value as Component).htmlAttributes.gridArea[2]
+    } / ${(activeComponentObj.value as Component).htmlAttributes.gridArea[3]};
 \tz-index: ${(activeComponentObj.value as Component).z};
 } \n`;
-}
-
-for (const html of htmlArray) {
-  if (html.class === " ") styleString = "";
-  if (html.class) {
-    styleString += `.${html.class} {\n height: ${html.h}%; \n width: ${html.w}%; \n top: ${html.x}%;
-     \n left: ${html.y}%; \n z-index: ${html.z};}\n`;
   }
-}
 
-// concat all code within script tags
-// if exportAsTypescript is on, out should be <script lang="ts">
-let output;
-if (exportAsTypescript.value === "on") {
-  output = "\n\n<script lang='ts'>\n";
-  output +=
-    vuetensilsImports +
-    imports +
-    "\nexport default defineComponent ({\n  name: '" +
-    componentName +
-    "';";
-} else {
-  output = "\n\n<script>\n";
-  output +=
-    vuetensilsImports +
-    imports +
-    "\nexport default {\n  name: '" +
-    componentName +
-    "'";
-}
-output += ",\n  components: {";
-output += childrenComponentNames + "  },\n";
-output += data;
-output += computed;
-output += methods;
+  for (const html of htmlArray) {
+    if (html.class === " ") styleString = "";
+    if (html.class) {
+      styleString += `.${html.class} {\n height: ${html.h}%; \n width: ${html.w}%; \n top: ${html.x}%;
+       \n left: ${html.y}%; \n z-index: ${html.z};}\n`;
+    }
+  }
+
+  // concat all code within script tags
+  // if exportAsTypescript is on, out should be <script lang="ts">
+  let output;
+  if (exportAsTypescript.value === "on") {
+    output = "\n\n<script lang='ts'>\n";
+    output +=
+      vuetensilsImports +
+      imports +
+      "\nexport default defineComponent ({\n  name: '" +
+      componentName +
+      "';";
+  } else {
+    output = "\n\n<script>\n";
+    output +=
+      vuetensilsImports +
+      imports +
+      "\nexport default {\n  name: '" +
+      componentName +
+      "'";
+  }
+  output += ",\n  components: {";
+  output += childrenComponentNames + "  },\n";
+  output += data;
+  output += computed;
+  output += methods;
 
   if (exportAsTypescript.value === "on") {
     if (activeComp.state.length || activeComp.actions.length) {
